f0d502be098b96d31b5fe70c36fa29649c76ab31
palomaalballay <119420704+palomaalballay@users.noreply.github.com>
2023-03-01 21:15:44 -0300

    Create ej-1.js

---
+++ ej-1.js
@@ -0,0 +1,10 @@
+const fecha_cadena = new Date("april 1 1997")
+console.log(fecha_cadena)
+
+const libro = {
+    nombre: "Fluir",
+    "Anyo de publicacion": 1990,
+    Autor: "Mihály Csíkszentmihályi",
+}
+
+const lista = ["Paloma Alballay", 25, true, Date("april 1 1997"), libro]
